c8290c088aa2a8ea41da125a0feccc93969de315
Denis <denis@denis.com>
2022-07-27 16:01:07 +0300

    chore: добавлена документация к API

--- src/api/v1/films.py
+++ src/api/v1/films.py
@@ -26,7 +26,9 @@ class FilmAPI(UUIDMixin, BaseModel):
     directors: list[Person]
 
 
-@router.get('/', response_model=list[FilmListAPI])
+@router.get('/',
+            response_model=list[FilmListAPI],
+            response_description='List of films')
 async def film_list(
     page_size: int = Query(10, description='Number of films on page'),
     page: int = Query(1, description='Page number'),
@@ -34,13 +36,19 @@ async def film_list(
                                       'of "field":"direction(=asc|desc)" '
                                       'pairs. Example: imdb_rating:desc)'),
     genre: str = Query(None, description='Filter by genre uuid'),
-    film_service: FilmService = Depends(get_film_service)
+    film_service: FilmService = Depends(get_film_service),
 ) -> list[FilmListAPI]:
+    """
+    Returns list of films by the parameters specified in the query.
+    Each element of the list is a dict of the FilmListAPI structure.
+    """
     films = await film_service.all(page_size=page_size, page=page, sort=sort, genre=genre)
     return [FilmListAPI.parse_obj(film.dict(by_alias=True)) for film in films]
 
 
-@router.get('/search', response_model=list[FilmListAPI])
+@router.get('/search',
+            response_model=list[FilmListAPI],
+            response_description='List of films')
 async def film_search(
     page_size: int = Query(10, description='Number of films on page'),
     page: int = Query(1, description='Page number'),
@@ -50,12 +58,25 @@ async def film_search(
     query: str = Query(None, description='Part of the movie title (Example: dark sta )'),
     film_service: FilmService = Depends(get_film_service)
 ) -> list[FilmListAPI]:
+    """
+    Returns list of films by the parameters specified in the query.
+    Each element of the list is a dict of the FilmListAPI structure.
+
+    Unlike the /films/ endpoint, it contains the "query" parameter.
+
+    Parameter **query**: part of film title.
+    """
     films = await film_service.all(page_size=page_size, page=page, sort=sort, query=query)
     return [FilmListAPI.parse_obj(film.dict(by_alias=True)) for film in films]
 
 
-@router.get('/{film_id}', response_model=FilmAPI)
+@router.get('/{film_id}',
+            response_model=FilmAPI,
+            response_description='Dict with all information about the film')
 async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)) -> FilmAPI:
+    """
+    Returns the dict with all information about the film by ID.
+    """
     film = await film_service.get_by_id(film_id)
     if not film:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
